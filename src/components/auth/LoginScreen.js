5701711c8f8a90f74f0e9bd969049d972675bf3a
Victor Colina <victorcolina2202@gmail.com>
2022-03-08 17:02:43 -0300

    Creación de carpeta components en styles para trabajar los estilos de los componentes por separado

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,6 +3,27 @@ import React from 'react';
 
 export const LoginScreen = () => {
     return (
-        <div>LoginScreen</div>
+        <>
+            <h3>Login</h3>
+
+            <form>
+                <input
+                    type="text"
+                    placeholder='Email'
+                    name='email' />
+
+                <input
+                    type="password"
+                    placeholder='Password'
+                    name='password' />
+
+                <button type='submit'>
+                    Login
+                </button>
+
+                <hr />
+                google
+            </form>
+        </>
     )
 }
