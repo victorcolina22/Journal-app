3158852234c5671429039dbc9e6eda1d9acd9a83
Victor Colina <victorcolina2202@gmail.com>
2022-03-11 20:55:37 -0300

    Mantener el estado de la autenticación al recargar implementada

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { firebase } from '../firebase/firebase-config';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 
 export const AppRouter = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            const { uid, displayName } = user;
+            // El signo "?" funciona en este caso preguntando primero que si el objeto "user"
+            // contiene algo evaluará si existe el "uid", si no dará false.
+            if (user?.uid) {
+                dispatch(login(uid, displayName));
+            }
+        })
+    }, []);
+
     return (
         <BrowserRouter>
             <div>
