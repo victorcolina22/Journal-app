d3df277d34ffe7dae8f0408c6eddbb5d9a95f30d
Victor Colina <victorcolina2202@gmail.com>
2022-03-15 12:16:18 -0300

    Optimización de la petición de carga de notas, cambio del dispatch de setnotes desde approuter a las acciones de notes.js

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,5 @@
 import { db } from "../firebase/firebase-config";
+import { loadNotes } from "../helpers/loadNotes";
 import { types } from "../types/types";
 
 
@@ -27,7 +28,14 @@ export const activeNote = (id, note) => ({
         id,
         ...note
     }
-})
+});
+
+export const startLoadingNotes = (uid) => {
+    return async (dispatch) => {
+        const notes = await loadNotes(uid);
+        dispatch(setNotes(notes));
+    }
+}
 
 export const setNotes = (notes) => ({
     type: types.notesLoad,
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 import { firebase } from '../firebase/firebase-config';
@@ -8,8 +8,7 @@ import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
-import { loadNotes } from '../helpers/loadNotes';
-import { setNotes } from '../actions/notes';
+import { startLoadingNotes } from '../actions/notes';
 
 
 export const AppRouter = () => {
@@ -30,8 +29,7 @@ export const AppRouter = () => {
             if (user?.uid) {
                 dispatch(login(user.uid, user.displayName));
                 setIsLoggedIn(true);
-                const notes = await loadNotes(user.uid);
-                dispatch(setNotes(notes));
+                dispatch(startLoadingNotes(user.uid));
             } else {
                 setIsLoggedIn(false);
             }
