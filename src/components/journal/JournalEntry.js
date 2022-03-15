2831eacf765b73167e052732ab300ab16ee1f24a
Victor Colina <victorcolina2202@gmail.com>
2022-03-15 13:02:35 -0300

    Instalación de moment.js para manejar formatos de fechas y renderizado de las notas en pantalla

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,30 +1,51 @@
 import React from 'react';
+import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, title, body, date, url }) => {
+
+    const dispatch = useDispatch();
+
+    const handleEntryClick = () => {
+        const notes = {
+            title,
+            body,
+            date,
+            url
+        }
+        dispatch(activeNote(id, notes));
+    }
+
     return (
-        <div className='journal__entry'>
-            <div
-                className="journal__entry-picture"
-                style={{
-                    backgroundImage: 'url(https://img.freepik.com/vector-gratis/hermoso-paisaje-primavera-degradado_23-2148448598.jpg?size=626&ext=jpg&ga=GA1.2.1522229396.1640476800)',
-                    backgroundSize: 'cover'
-                }}>
+        <div
+            className='journal__entry'
+            onClick={handleEntryClick}>
+            {
+                url &&
+                <div
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundImage: `url(${url})`,
+                        backgroundSize: 'cover'
+                    }}>
 
-            </div>
+                </div>
+            }
 
             <div className="journal__entry-body">
                 <p className='journal__entry-title'>
-                    Un nuevo día
+                    {title}
                 </p>
                 <p className='journal__entry-content'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore deleniti tenetur reprehenderit placeat at maiores? Repellendus, corrupti. Aspernatur corrupti quisquam non dolorem quia, vero illo necessitatibus corporis quidem dolorum voluptates?
+                    {body}
                 </p>
             </div>
 
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{moment(date).format('dddd')}</span>
+                <h4>{moment(date).format('D')}</h4>
             </div>
         </div>
     )
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 
 export const NoteScreen = () => {
+    const { active: note } = useSelector(state => state.notes);
+
+    const [formValues, handleInputChange] = useForm(note);
+    const { body, title } = formValues;
+
     return (
         <div className='notes__main-content'>
             <NotesAppBar />
@@ -12,18 +19,27 @@ export const NoteScreen = () => {
                     className='notes__title-input'
                     type="text"
                     placeholder='Some awesome title'
+                    name='title'
+                    onChange={handleInputChange}
+                    value={title}
                     autoComplete='off' />
 
                 <textarea
                     className='notes__textarea'
-                    placeholder='What happened today'>
+                    placeholder='What happened today'
+                    name='body'
+                    onChange={handleInputChange}
+                    value={body}>
                 </textarea>
 
-                <div className="notes__images">
-                    <img
-                        src="https://www.kimley-horn.com/wp-content/uploads/2021/04/Centennial-Lakes-Bridge-at-Night.jpg"
-                        alt="arboles" />
-                </div>
+                {
+                    (note.url) &&
+                    <div className="notes__images">
+                        <img
+                            src="https://www.kimley-horn.com/wp-content/uploads/2021/04/Centennial-Lakes-Bridge-at-Night.jpg"
+                            alt="arboles" />
+                    </div>
+                }
             </div>
         </div>
     )
