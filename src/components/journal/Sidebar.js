0c5270495a67e426584696d962ecc094bb589159
Victor Colina <victorcolina2202@gmail.com>
2022-03-15 10:52:03 -0300

    Cambio de nombre en sidebar por nombre almacenado en el store de redux

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 import { JournalEntries } from './JournalEntries';
 
 
 export const Sidebar = () => {
+    const { name } = useSelector(state => state.auth);
     const dispatch = useDispatch();
 
     const handleLogout = () => {
@@ -16,7 +17,7 @@ export const Sidebar = () => {
             <div className='journal__sidebar-navbar'>
                 <h3 className='mt-5'>
                     <i className='far fa-moon'></i>
-                    <span> Victor</span>
+                    <span> {name}</span>
                 </h3>
 
                 <button
