fff71c48e344d4496d39c87c491237189d2300cb
Victor Colina <victorcolina2202@gmail.com>
2022-03-15 22:08:10 -0300

    Cambio en componente NothingSelected

--- src/components/journal/NothingSelected.js
+++ src/components/journal/NothingSelected.js
@@ -5,9 +5,7 @@ export const NothingSelected = () => {
     return (
         <div className='nothing__main-content'>
             <p>
-                Select something
-                <br />
-                pr create an entry!
+                Create a new entry!
             </p>
 
             <i className='far fa-star fa-4x mt-5'></i>
